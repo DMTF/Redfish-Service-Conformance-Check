54b490c1057e35db23c04d7ef4a84d5dcb973ea0
Robin Ronson <robin_ronson@yahoo.com>
2018-03-28 01:05:47 -0500

    added bootoption test

---
+++ rfs_test/TEST_bootoption_schema.py
@@ -0,0 +1,17 @@
+# Copyright Notice:
+# Copyright 2016-2018 Distributed Management Task Force, Inc. All rights reserved.
+# License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/Redfish-Service-Conformance-Check/blob/master/LICENSE.md
+
+#####################################################################################################
+# File: TEST_bootoption_schema.py
+# Description: Redfish service conformance check tool. This module contains implemented assertions for
+#   SUT.These assertions are based on operational checks on Redfish Service to verify that it conforms
+#   to the normative statements from the Redfish specification.
+#   See assertions in redfish-assertions.xlxs for assertions summary
+#
+# Verified/operational Python revisions (Windows OS) :
+#       3.5.2
+#
+# Initial code released : 03/2018
+#   Robin Thekkadathu Ronson ~ Texas Tech University
+#####################################################################################################
