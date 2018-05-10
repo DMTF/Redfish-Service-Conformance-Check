c462a6bba687a9b5bf5cda665d6194258d8c13a9
l1mb0 <robin_ronson@yahoo.com>
2018-05-10 12:00:20 -0500

    Removed Attributes files and added base codes for BOOT102 and COLL103 assertions.

--- rfs_test/TEST_bootoption_schema.py
+++ rfs_test/TEST_bootoption_schema.py
@@ -15,3 +15,49 @@
 # Initial code released : 05/2018
 #   Robin Thekkadathu Ronson ~ Texas Tech University
 #####################################################################################################
+
+###################################################################################################
+# Name: Assertion BOOT102(self,log)
+# Description:
+# The value of this property shall indicate if the Boot Option is enabled.  If this property is 
+# set to false, the Boot Option referenced in the Boot Order array found on the Computer System 
+# shall be skipped. In the UEFI context, this property shall influence the Load Option Active flag 
+# for the Boot Option.
+###################################################################################################
+def Assertion_BOOT102(self,log) :
+    log.AssertionID = 'BOOT102'
+    assertion_status = log.PASS
+    log.assertion_log('BEGIN_ASSERTION', None)
+    relative_uris = self.relative_uris
+    authorization = 'on'
+    rq_headers = self.request_headers()
+
+    root_link_key = 'Systems'    
+
+    json_payload, headers, status = self.http_GET(self.sut_toplevel_uris[root_link_key]['url'], rq_headers, authorization)
+
+    if status == 200: 
+          try: 
+            authorization = 'on'
+            json_payload, headers, status = self.http_GET('/redfish/v1/Managers/MultiBladeBMC/LogServices/Log/Entries/', rq_headers, authorization)
+            log_collection = (json_payload['Members'])
+
+          except: 
+            assertion_status = log.FAIL
+            log.assertion_log('line', "Failure attempt was not logged in after failed login attempts reached the threshold referenced by AccountLockoutThreshold")
+    else
+        assertion_status = log.FAIL
+        log.assertion_log('line', "The systems resource was not found.")
+
+    log.assertion_log(assertion_status, None)
+    return (assertion_status)
+
+#
+## end Assertion BOOT102
+
+# run(self, log):
+# Takes sut obj and logger obj 
+###################################################################################################
+def run(self, log): 
+      
+    assertion_status = Assertion_BOOT102(self,log)
--- rfs_test/TEST_collectioncapabilities_schema.py
+++ rfs_test/TEST_collectioncapabilities_schema.py
@@ -15,3 +15,40 @@
 # Initial code released : 05/2018
 #   Robin Thekkadathu Ronson ~ Texas Tech University
 #####################################################################################################
+
+###################################################################################################
+# Name: Assertion COLL103(self,log)
+# Description:
+# The value of this property shall be a reference to a Resource that matches the type for the given 
+# collection and shall contain annotations that describe the properties allowed in the POST request.
+###################################################################################################
+def Assertion_COLL103(self,log) :
+    log.AssertionID = 'COLL103'
+    assertion_status = log.PASS
+    log.assertion_log('BEGIN_ASSERTION', None)
+    relative_uris = self.relative_uris
+    print('Checking if all responses are in JSON')
+    authorization = 'on'
+    rq_headers = self.request_headers()
+
+    for relative_uri in relative_uris:
+        json_payload, headers, status = self.http_GET(relative_uris[relative_uri], rq_headers, authorization)
+
+        if status == 200 :
+            if not json_payload:
+                print ('The response received is not in JSON %s'% json_payload)
+                assertion_status = log.FAIL
+        else :
+            continue
+    log.assertion_log(assertion_status, None)
+    return (assertion_status)
+
+#
+## end Assertion BOOT102
+
+# run(self, log):
+# Takes sut obj and logger obj 
+###################################################################################################
+def run(self, log): 
+      
+    assertion_status = Assertion_COLL103(self,log)
