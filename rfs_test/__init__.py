1621cfc52bbb41555bec08f6c06e871c8a9e8f1d
Mike Raineri <michael.raineri@dell.com>
2018-09-07 14:03:39 -0400

    Copyright update

--- rfs_test/__init__.py
+++ rfs_test/__init__.py
@@ -1,5 +1,5 @@
 # Copyright Notice:
-# Copyright 2016-2017 Distributed Management Task Force, Inc. All rights reserved.
+# Copyright 2016-2017 DMTF. All rights reserved.
 # License: BSD 3-Clause License. For full text see link: https://github.com/DMTF/Redfish-Service-Conformance-Check/blob/master/LICENSE.md
 
 ###################################################################################################
